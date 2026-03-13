b9dfe79811614bfeeaa6498894614abe4b9e9d80
arshi khan <72272236+arshikhan229@users.noreply.github.com>
2026-03-13 13:59:56 +0500

    Add install_requires for frappe dependency

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, find_packages
 
+
 setup(
     name="zikpro_invoice_ocr",
     version="0.0.2",
@@ -9,5 +10,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[]
+    install_requires=[
+        "frappe>=15.0.0",
+    ],
 )
